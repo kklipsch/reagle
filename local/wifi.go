1315067fbc5e09c2a35ba47e9fde60f806c44621
kklipsch <kklipsch@example.com>
2018-10-28 14:18:01 +0000

    better wifi json

--- local/wifi.go
+++ local/wifi.go
@@ -9,11 +9,11 @@ func NewWifiStatusCommand() Command {
 
 //WifiStatus is the response from the wifi_status command
 type WifiStatus struct {
-	XMLName    xml.Name `xml:"WiFiStatus"`
-	Enabled    string
-	Type       string
-	SSID       string
-	Encryption string
-	Channel    string
-	IPAddress  string `xml:"IpAddress"`
+	XMLName    xml.Name `xml:"WiFiStatus" json:"-"`
+	Enabled    string   `json:"enabled"`
+	Type       string   `json:"type"`
+	SSID       string   `json:"ssid"`
+	Encryption string   `json:"encryption"`
+	Channel    string   `json:"channel"`
+	IPAddress  string   `xml:"IpAddress" json:"ip_address"`
 }
