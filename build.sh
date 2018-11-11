4fc1718e701932e96980e21c85dba9e13d0dd106
kklipsch <kklipsch@example.com>
2018-11-11 14:55:18 +0000

    add improved firmware flag to dockerfile/build for testing

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker build -t kklipsch/reagled --build-arg TEST_FLAG=$1 --build-arg REAGLE_LOCAL_LOCATION=$REAGLE_LOCAL_LOCATION --build-arg REAGLE_LOCAL_USER=$REAGLE_LOCAL_USER --build-arg REAGLE_LOCAL_PASSWORD=$REAGLE_LOCAL_PASSWORD --no-cache .
+TEST_FLAG=$1
+
+docker build -t kklipsch/reagled --build-arg TEST_FLAG=$TEST_FLAG --build-arg REAGLE_LOCAL_LOCATION=$REAGLE_LOCAL_LOCATION --build-arg REAGLE_LOCAL_USER=$REAGLE_LOCAL_USER --build-arg REAGLE_LOCAL_PASSWORD=$REAGLE_LOCAL_PASSWORD --build-arg REAGLE_IMPROVED_FIRMWARE=$REAGLE_IMPROVED_FIRMWARE --no-cache .
