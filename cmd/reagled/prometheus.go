0d6b627f5f5c630487c2608a825d719dbd3706c4
kklipsch <kklipsch@example.com>
2019-02-25 23:20:23 +0000

    switch to promauto

--- cmd/reagled/prometheus.go
+++ cmd/reagled/prometheus.go
@@ -5,21 +5,22 @@ import (
 	"net/http"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	errorsCount = prometheus.NewCounter(prometheus.CounterOpts{
+	errorsCount = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "errors",
 		Help: "Count of non-fatal errors",
 	})
 
-	requestsInFlightGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+	requestsInFlightGauge = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "in_flight_requests",
 		Help: "A gauge of requests currently being served.",
 	})
 
-	requestsCount = prometheus.NewCounterVec(
+	requestsCount = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "api_requests_total",
 			Help: "Count of api requests",
@@ -27,7 +28,7 @@ var (
 		[]string{"handler", "code", "method"},
 	)
 
-	requestsDuration = prometheus.NewHistogramVec(
+	requestsDuration = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "request_duration_seconds",
 			Help:    "A histogram of latencies for requests.",
@@ -36,7 +37,7 @@ var (
 		[]string{"handler", "code", "method"},
 	)
 
-	requestSize = prometheus.NewHistogramVec(
+	requestSize = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "request_size_bytes",
 			Help:    "A histogram of request sizes for requests.",
@@ -45,7 +46,7 @@ var (
 		[]string{"handler", "code", "method"},
 	)
 
-	responseSize = prometheus.NewHistogramVec(
+	responseSize = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "response_size_bytes",
 			Help:    "A histogram of response sizes for requests.",
@@ -54,11 +55,6 @@ var (
 		[]string{"handler", "code", "method"},
 	)
 )
-
-func init() {
-	prometheus.MustRegister(errorsCount)
-	prometheus.MustRegister(requestsInFlightGauge, requestsCount, requestsDuration, requestSize, responseSize)
-}
 
 func instrumentHandler(handlerName string, h http.Handler) http.Handler {
 	return promhttp.InstrumentHandlerInFlight(requestsInFlightGauge,
