52ddd0e221e83237af73e3dcecbb38c75c22f363
kklipsch <junk@klipsch.net>
2019-02-27 17:33:35 -0600

    minor cleanup of entrypoint (#10)
    * make flags unexported so lint and the like dont complain
    
    * introduce iota for return codes

--- cmd/reagled/main.go
+++ cmd/reagled/main.go
@@ -18,39 +18,39 @@ import (
 var (
 	applicationLogger = log.WithFields(log.Fields{"name": "reagled"})
 
-	AddressFlag = cli.StringFlag{
+	addressFlag = cli.StringFlag{
 		Name:   "address",
 		Usage:  "where to serve the endpoints",
 		EnvVar: "REAGLED_ADDRESS",
 		Value:  ":9000",
 	}
 
-	WaitFlag = cli.DurationFlag{
+	waitFlag = cli.DurationFlag{
 		Name:   "wait",
 		Usage:  "how much time to ensure between calls to the eagle",
 		EnvVar: "REAGLED_WAIT",
 		Value:  time.Second,
 	}
 
-	LocationFlag = cli.StringFlag{
+	locationFlag = cli.StringFlag{
 		Name:   "location",
 		Usage:  "eagle address",
 		EnvVar: local.LocationEnv,
 	}
 
-	UserFlag = cli.StringFlag{
+	userFlag = cli.StringFlag{
 		Name:   "user",
 		Usage:  "eagle user",
 		EnvVar: local.UserEnv,
 	}
 
-	PasswordFlag = cli.StringFlag{
+	passwordFlag = cli.StringFlag{
 		Name:   "password",
 		Usage:  "eagle password",
 		EnvVar: local.PasswordEnv,
 	}
 
-	ModelIDFlag = cli.StringFlag{
+	modelIDFlag = cli.StringFlag{
 		Name:   "model_id",
 		Usage:  "what the eagle is reporting for your smart meter model id, can be found by hitting the device_list endpoint. Unlikely to need to be set",
 		EnvVar: local.MeterModelIDEnv,
@@ -58,34 +58,34 @@ var (
 
 	//oddity of the cli parsing library.  a boolt will be set to true by default, so 'setting' this turns it off, giving a mismatch between the name and
 	//the cli ergonomics
-	ImprovedFirmwareFlag = cli.BoolTFlag{
+	improvedFirmwareFlag = cli.BoolTFlag{
 		Name:   "unimproved_firmware",
 		Usage:  "if your eagle has the unimproved firmware (it responds with invalid xml for multiplier & divisor queries) this should be set",
 		EnvVar: local.ImprovedFirmwareEnv,
 	}
 
-	DebugRequestFlag = cli.BoolFlag{
+	debugRequestFlag = cli.BoolFlag{
 		Name:   "debug_request",
 		Usage:  "if set requests will be debugged",
 		EnvVar: local.DebugRequestEnv,
 	}
 
-	DebugResponseFlag = cli.BoolFlag{
+	debugResponseFlag = cli.BoolFlag{
 		Name:   "debug_response",
 		Usage:  "if set responses will be debugged",
 		EnvVar: local.DebugResponseEnv,
 	}
 
 	flags = []cli.Flag{
-		AddressFlag,
-		WaitFlag,
-		LocationFlag,
-		UserFlag,
-		PasswordFlag,
-		ModelIDFlag,
-		ImprovedFirmwareFlag,
-		DebugRequestFlag,
-		DebugResponseFlag,
+		addressFlag,
+		waitFlag,
+		locationFlag,
+		userFlag,
+		passwordFlag,
+		modelIDFlag,
+		improvedFirmwareFlag,
+		debugRequestFlag,
+		debugResponseFlag,
 	}
 )
 
@@ -106,6 +106,13 @@ func main() {
 	}
 }
 
+const (
+	configureErrorCode int = iota
+	mediatorErrorCode
+	bridgeErrorCode
+	shutdownErrorCode
+)
+
 func start(cliCtx *cli.Context) error {
 	applicationLogger.Infoln("starting up")
 
@@ -114,7 +121,7 @@ func start(cliCtx *cli.Context) error {
 	config, err := configure(ctx, cliCtx)
 	if err != nil {
 		err = fmt.Errorf("error configuring: %v", err)
-		return cli.NewExitError(err, 2)
+		return cli.NewExitError(err, configureErrorCode)
 	}
 
 	applicationLogger.WithFields(log.Fields{"config": config}).Infoln("configured")
@@ -122,13 +129,13 @@ func start(cliCtx *cli.Context) error {
 	mediator, err := startAPIMediator(ctx, config)
 	if err != nil {
 		err = fmt.Errorf("error starting api mediator: %v", err)
-		return cli.NewExitError(err, 3)
+		return cli.NewExitError(err, mediatorErrorCode)
 	}
 
 	_, err = newPrometheusBridge(ctx, prometheus.DefaultRegisterer, mediator)
 	if err != nil {
 		err = fmt.Errorf("error creating prometheus bridge: %v", err)
-		return cli.NewExitError(err, 4)
+		return cli.NewExitError(err, bridgeErrorCode)
 	}
 
 	srv := startServer(config, mediator)
@@ -143,7 +150,7 @@ func start(cliCtx *cli.Context) error {
 	err = srv.Shutdown(shutdownCtx)
 	if err != nil {
 		err = fmt.Errorf("error shutting down web server: %v", err)
-		return cli.NewExitError(err, 5)
+		return cli.NewExitError(err, shutdownErrorCode)
 	}
 
 	applicationLogger.Infoln("done")
