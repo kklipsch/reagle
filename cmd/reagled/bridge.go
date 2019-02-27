4c88b70b824dbaa249a65211da0b01cc05850dac
kklipsch <kklipsch@example.com>
2019-02-27 23:22:10 +0000

    fix up bridge and wire it up

--- cmd/reagled/bridge.go
+++ cmd/reagled/bridge.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"sync/atomic"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -19,48 +22,54 @@ type (
 		//context necessary for api calls but no great way to inject it in prometheus collector interface
 		contextFactory func() context.Context
 
-		//hardware address for smart meter
-		hardwareAddress string
-
 		//prom documentation makes it seem like you have to return the same metrcis
 		//caching previous to return in case of error
-		previousValues metricValues
+		previousValues atomic.Value
+
+		//use the standard mediator that keeps things threadsafe and throttled
+		mediator apiMediator
 	}
 )
 
-func newPrometheusBridge(ctx context.Context, reg prometheus.Registerer, hardwareAddress string) (rainForestBridge, error) {
-	bridge := rainForestBridge{
-		contextFactory:  func() context.Context { return ctx },
-		hardwareAddress: hardwareAddress,
+func newPrometheusBridge(ctx context.Context, reg prometheus.Registerer, mediator apiMediator) (*rainForestBridge, error) {
+	bridge := &rainForestBridge{
+		contextFactory: func() context.Context { return ctx },
+		mediator:       mediator,
 	}
+
+	bridge.previousValues.Store(metricValues{})
 
 	err := reg.Register(bridge)
 	return bridge, err
 }
 
 //we have constant metrics so this can delegate to library code
-func (bridge rainForestBridge) Describe(ch chan<- *prometheus.Desc) {
+func (bridge *rainForestBridge) Describe(ch chan<- *prometheus.Desc) {
 	prometheus.DescribeByCollect(bridge, ch)
 }
 
 //collect makes the call to the api and converts into prometheus metrics
-func (bridge rainForestBridge) Collect(ch chan<- prometheus.Metric) {
-	/*
-		ctx := bridge.contextFactory()
+func (bridge *rainForestBridge) Collect(ch chan<- prometheus.Metric) {
+	ctx := bridge.contextFactory()
 
-		metricValues, err := getMetricValues(ctx, api, bridge.hardwareAddress)
-		if err != nil {
-			errorsCount.Inc()
-			log.Errorf("error getting metrics:%v", err)
-			collectValues(ctx, ch, bridge.previousValues)
-		} else {
-			collectValues(ctx, ch, bridge.previousValues)
-			bridge.previousValues = metricValues
-		}
-	*/
+	timeout, clean := context.WithTimeout(ctx, time.Second*5)
+	defer clean()
+
+	response, err := bridge.mediator.sendReceive(timeout, newAPIRequest(baseMetrics, ""))
+	if err != nil {
+		errorsCount.Inc()
+		applicationLogger.WithFields(log.Fields{"err": err}).Errorln("unable to get metrics for prometheus bridge")
+		response = bridge.previousValues.Load()
+	}
+
+	collectValues(ctx, ch, response)
+	bridge.previousValues.Store(response)
 }
 
-func collectValues(ctx context.Context, ch chan<- prometheus.Metric, values metricValues) {
+func collectValues(ctx context.Context, ch chan<- prometheus.Metric, response interface{}) {
+	//go ahead and panic cause if this isnt a metricvalues it means something disastorous has happened
+	values := response.(metricValues)
+
 	send(ctx, ch, prometheus.MustNewConstMetric(
 		instantDemand,
 		prometheus.GaugeValue,
