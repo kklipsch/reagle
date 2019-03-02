e9a8350846e35cba9422b8080217801fb6df7a36
kklipsch <kklipsch@example.com>
2019-03-02 18:16:18 +0000

    fix test server

--- client/mediate_test.go
+++ client/mediate_test.go
@@ -11,15 +11,13 @@ import (
 )
 
 func TestMediateWifiStatus(t *testing.T) {
-	t.Skip("TODO: make this work")
-
 	ctx, clean := context.WithTimeout(context.Background(), time.Second)
 	defer clean()
 
-	ts := local.StartTestServer(local.ServeWifiStatus(local.WifiStatus{Enabled: "enabled", SSID: "ssid"}))
+	ts, config := local.StartTestServer(local.ServeWifiStatus(local.WifiStatus{Enabled: "enabled", SSID: "ssid"}))
 	defer ts.Close()
 
-	api := local.New(local.Config{Location: ts.URL})
+	api := local.New(config)
 	mediator := newMediator(api, time.Second)
 
 	result, err := mediator.query(ctx, localWifiStatus, nil)
